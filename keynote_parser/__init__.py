b62299019507d29b099bd43be1f4f9a83224bde0
Peter Sobot <psobot@gmail.com>
2022-04-23 19:44:01 -0400

    Update __init__.py

--- keynote_parser/__init__.py
+++ keynote_parser/__init__.py
@@ -5,7 +5,7 @@ __author__ = "Peter Sobot"
 import keynote_parser.macos_app_version
 
 __major_version__ = 1
-__patch_version__ = 2
+__patch_version__ = 0
 __supported_keynote_version__ = keynote_parser.macos_app_version.MacOSAppVersion(
     "12.0", "7033.0.134", "1A143"
 )
